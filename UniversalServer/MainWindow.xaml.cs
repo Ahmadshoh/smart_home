044ac427e5473ea6da170058dcd0ee40b87632df
Ahmadshoh Nasrullozoda <a.nasrullozoda@AD.ITS-STUTTGART.LOGODIDACT.NET>
2022-03-17 10:46:08 +0100

    basic mysql connection

--- UniversalServer/MainWindow.xaml.cs
+++ UniversalServer/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace UniversalServer
     {
         public MainWindow()
         {
+           
             InitializeComponent();
             this.DataContext = new MainViewModel();
         }
--- UniversalServer/Model/DBAccess.cs
+++ UniversalServer/Model/DBAccess.cs
@@ -5,23 +5,76 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
+using MySql.Data.MySqlClient;
+
 namespace UniversalServer.Model
 {
     public delegate EventHandler ErrorEventHandler(string msg);
     public class DBAccess
     {
+        MySqlConnection con = new MySqlConnection(@"SERVER = localhost;DATABASE=smarthome;UID=root;PASSWORD=;");
 
-        public DBAccess()
+        public bool openConnection()
         {
-            //Hier die Verbindung zur DB herstellen. Siehe Info-Pool
-            //Verbindungsdaten dürfen "hard codiert" werden.
+            try
+            {
+                con.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
 
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        public bool closeConnection()
+        {
+            try
+            {
+                con.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
 
         ///Diese Methode für den Datensatz in die Datenbank ein. Siehe Info-Pool      
         public void InsertData(TempValue t, HumidValue h, PressureValue p, DateTime dt, string ipa)
         {
+            string query = "INSERT INTO wetterwerte " +
+                "(temperatur, datetime, humidity, druck, sender_ip) " +
+                "VALUES(" + t + ", " + dt + ", " + h + ", " + p + ", " + ipa + ")";
 
+            //open connection
+            if (this.openConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, con);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.closeConnection();
+            }
 
         }
 
